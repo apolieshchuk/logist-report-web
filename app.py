8e31284831aa67e0c10fd76bb7adbba80648175d
apolieshchuk <anton2013ua@gmail.com>
2019-06-19 17:56:35 +0300

    190619_logist-report-web

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask,render_template
+import sqlite3
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    with sqlite3.connect("auto.db") as conn:
+        conn.row_factory = dict_factory
+        db = conn.cursor()
+        rows = db.execute("SELECT * FROM mytable")
+        # print(rows.fetchall())
+        return render_template("index.html", rows = rows.fetchall())
 
 
 if __name__ == '__main__':
     app.run()
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
