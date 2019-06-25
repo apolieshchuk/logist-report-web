ca02668a6099852862bd5d0c97fecdb357081f69
apolieshchuk <anton2013ua@gmail.com>
2019-06-25 17:12:29 +0300

    190625(17:12)_logist-report-web

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 from flask import Flask,render_template
-import sqlite3
-
+import mysql.connector
 app = Flask(__name__)
 
 
 
 @app.route('/')
 def index():
-    with sqlite3.connect("auto.db") as conn:
-        # conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
+    conn = mysql.connector.connect(user='root', password='aipx123',
+                                   host='localhost',port=3306,
+                                   database='auto')
+    cursor = conn.cursor()
+    sql = "SELECT * FROM test"
+    cursor.execute(sql)
+    for (col1,col2,col3,col4) in cursor:
+        print(col1,col2,col3,col4)
+    return "Hello wotrld!"
 
-        # шапка таблицы
-        info = cur.execute("PRAGMA table_info(mytable)")
-        head = [el[1] for el in info.fetchall()]
-
-        # ширина столбцов таблицы
-        # TODO CSS
-
-        rows = cur.execute("SELECT * FROM mytable")
-        # print(rows.fetchall())
-        return render_template("index.html", rows = rows.fetchall(), head = head)
+    # with sqlite3.connect("auto.db") as conn:
+    #     # conn.row_factory = sqlite3.Row
+    #     cur = conn.cursor()
+    #
+    #     # шапка таблицы
+    #     info = cur.execute("PRAGMA table_info(mytable)")
+    #     head = [el[1] for el in info.fetchall()]
+    #
+    #     # ширина столбцов таблицы
+    #     # TODO CSS
+    #
+    #     rows = cur.execute("SELECT * FROM mytable")
+    #     # print(rows.fetchall())
+    #     return render_template("index.html", rows = rows.fetchall(), head = head)
 
 
 if __name__ == '__main__':
